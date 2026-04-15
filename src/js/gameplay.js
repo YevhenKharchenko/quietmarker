06d4303917b1a586aef9cdc501b6331088621ddf
YevhenKharchenko <evgenvalerych@gmail.com>
2026-04-15 09:45:00 +0300

    Initial commit

---
+++ src/js/gameplay.js
@@ -0,0 +1,33 @@
+import Swiper from 'swiper';
+import 'swiper/css/bundle';
+
+let gameplaySwiper;
+
+gameplaySwiper = new Swiper('.gameplay-swiper-container', {
+  direction: 'horizontal',
+  loop: false,
+  centeredSlides: true,
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  initialSlide: 0,
+  spaceBetween: 20,
+  speed: 500,
+  allowTouchMove: true,
+  grabCursor: true,
+  breakpoints: {
+    1440: {
+      slidesPerView: 4,
+      centeredSlides: false,
+      allowTouchMove: false,
+      grabCursor: false,
+      spaceBetween: 0,
+    },
+  },
+  on: {
+    init(swiper) {
+      document
+        .querySelector('.gameplay-swiper-container')
+        .classList.add('show');
+    },
+  },
+});
